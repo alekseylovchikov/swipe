6d6b3d65371b75ccc72d929a3dc468ddc056b42e
Aleksey Lovchikov <alelov@brabdev.com>
2017-07-22 10:54:47 +0300

    force swipe left and right

--- src/Deck.js
+++ src/Deck.js
@@ -8,6 +8,7 @@ import {
 
 const SCREEN_WIDTH = Dimensions.get('window').width;
 const SWIPE_THRESHOLD = 0.25 * SCREEN_WIDTH;
+const SWIPE_OUT_DURATION = 250;
 
 class Deck extends Component {
 	constructor(props) {
@@ -21,9 +22,9 @@ class Deck extends Component {
 			},
 			onPanResponderRelease: (event, gesture) => {
 				if (gesture.dx > SWIPE_THRESHOLD) {
-					console.log('SWIPE RIGHT');
+					this.forceSwipeRight();
 				} else if (gesture.dx < -SWIPE_THRESHOLD) {
-					console.log('SWIPE LEFT');
+					this.forceSwipeLeft();
 				} else {
 					this.resetPosition();
 				}
@@ -31,6 +32,20 @@ class Deck extends Component {
 		});
 
 		this.state = { panResponder, position };
+	}
+
+	forceSwipeRight() {
+		Animated.timing(this.state.position, {
+			toValue: { x: SCREEN_WIDTH, y: 0 },
+			duration: SWIPE_OUT_DURATION
+		}).start();
+	}
+
+	forceSwipeLeft() {
+		Animated.timing(this.state.position, {
+			toValue: { x: -SCREEN_WIDTH, y: 0 },
+			duration: SWIPE_OUT_DURATION
+		}).start();
 	}
 
 	resetPosition() {
