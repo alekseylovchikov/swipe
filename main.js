52773c88303b0491442d58c7f0b8079f7ad90d90
Aleksey Lovchikov <alelov@brabdev.com>
2017-08-05 16:08:14 +0300

    15 - hide cards

--- main.js
+++ main.js
@@ -62,7 +62,7 @@ const styles = StyleSheet.create({
     flex: 1,
     backgroundColor: '#fff',
     paddingTop: 10,
-  },
+  }
 });
 
 Expo.registerRootComponent(App);
--- src/Deck.js
+++ src/Deck.js
@@ -86,15 +86,19 @@ class Deck extends Component {
 				return (
 					<Animated.View
 						key={index}
-						style={this.getCardStyle()}
+						style={[this.getCardStyle(), styles.cardStyle]}
 						{...this.state.panResponder.panHandlers}
 					>
 						{this.props.renderCard(card)}
 					</Animated.View>
 				);
 			}
-			return this.props.renderCard(card);
-		});
+			return (
+				<View key={card.id} style={styles.cardStyle}>
+					{this.props.renderCard(card)}
+				</View>
+			);
+		}).reverse();
 	}
 
 	render() {
@@ -105,5 +109,12 @@ class Deck extends Component {
 		);
 	}
 }
+
+const styles = {
+	cardStyle: {
+		position: 'absolute',
+		width: SCREEN_WIDTH,
+	}
+};
 
 export default Deck;
